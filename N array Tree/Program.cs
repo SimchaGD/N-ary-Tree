5bc0cc305635c10d082c34c0ab34543a4c1f860f
Simcha <simchavanhelvoort@gmail.com>
2019-02-06 15:01:00 +0100

    Opdracht in comments gezet

--- N array Tree/Program.cs
+++ N array Tree/Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*Implementeer een Tree:
+ *  De Tree is N-ary
+ *  Gebruik 2 classes: TreeNode en Tree
+ *  Elke TreeNode bevat een waarde van type T
+ *  Implementeer de volgende properties op Tree:
+ *      Count (Geef het aan Nodes van de Tree)
+ *      LeafCount (Geef het aantal leaf Nodes in de Tree)
+ *  Implementeer de volgende methods op Tree:
+ *      AddChildNode(parentNode, value)
+ *      [Voegt een nieuwe TreeNode toe met een waarde onder de parent TreeNode]
+ *      RemoveNode(node)
+ *      [Verwijder TreeNode met onderliggende Nodes]
+ *      TraverseNodes()
+ *      [Geeft alle node waardes terug (met iterator)]
+ *      SumToLeafs()
+ *      [Geeft de som van alle onderliggende nodes terug]
+ *  
+ *  Schrijf voor elk non-triviale class die je maakt een NUnit class met unit tests]
+ *  
+ *  Zet je uitwerking op github met twee projecten
+*/
+
+
 namespace N_array_Tree
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
